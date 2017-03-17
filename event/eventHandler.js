1f12cf265093e240aba716805bbee83262a8a428
donghyunkim <donghyunkim@donghyunkimui-MacBook-Pro.local>
2017-03-17 18:09:25 +0900

    [1.수정] 의존성을 줄이는 작업
    각 컴포넌트간의 의존성을 줄이는 작업
    이벤트 핸들러 수정중

--- event/eventHandler.js
+++ event/eventHandler.js
@@ -5,13 +5,32 @@
 //이벤트 핸들 부분
 
 //리스트 클릭 핸들러
-function listClickHandler(newsModelList) {
-    let contentDom = utility.$selector("#newsContents");
-    let headerDom = utility.$selector("header");
+
+
+
+
+//let contentDom = utility.$selector("#newsContents");
+//let headerDom = utility.$selector("header");
+//contentsView.render(modelList, contentDom,key,removeNewsClickHandler);
+
+// 돔, rendering view, 리스너 -> 객체로 보낸다 {viewObj , domObj, handlerFn }
+
+// let key =  Number(event.target.id);
+function listClickHandler(modelList) {
     let key =  Number(event.target.id);
-    menuView.renderMenu(newsModelList,headerDom,key,arrowClickHandler);
-    highLight(key);
-    contentsView.renderContents(newsModelList, contentDom,key,removeNewsClickHandler);
+    let argArr = Array.prototype.slice.call(arguments);
+    for(let i = 1; i < argArr.length-1; i++){
+        if(Object.prototype.toString.call(argArr[i]) !== "object"){
+            break;
+        }
+        let viewObj =  argArr[i].viewObj;
+        let domObj = argArr[i].domObj;
+        let handlerFn = argArr[i].handlerFn;
+        viewObj.render(modelList,domObj,key,handlerFn);
+    }
+    if(argArr[argArr.length-2] !== event) {
+        highLight(key);
+    }
 }
 
 //prev,next 버튼 클릭 핸들러
@@ -38,26 +57,19 @@ function arrowClickHandler (newsModelList){
     let selectedKey = newsModelList.getNewsKey(currentSite-1);
     let contentDom = utility.$selector("#newsContents");
     let headerDom = utility.$selector("header");
-    contentsView.renderContents(newsModelList,contentDom,selectedKey,removeNewsClickHandler);
+    contentsView.render(newsModelList,contentDom,selectedKey,removeNewsClickHandler);
     highLight(selectedKey);
-    menuView.renderMenu(newsModelList,headerDom,selectedKey,arrowClickHandler);
+    menuView.render(newsModelList,headerDom,selectedKey,arrowClickHandler);
 
 }
 
 //구독취소버튼 클릭 핸들러
 function removeNewsClickHandler(newsModelList){
     let key = Number(utility.$selector("hidden").id);
-
     newsModelList.deleteNews(key);
-
-
-
-
     let titleListDom = utility.$selector("#titleList");
     let contentDom = utility.$selector("#newsContents");
     let headerDom = utility.$selector("header");
-
-
 
     // 전체페이지를 렌더링 해야한다.
     let selectedKey = 0;
@@ -67,10 +79,16 @@ function removeNewsClickHandler(newsModelList){
     }else{
         selectedKey = newsModelList.getNewsModelList()[0].newsKey;
     }
-    menuView.renderMenu(newsModelList,headerDom,selectedKey,arrowClickHandler);
-    titleListView.renderTitleList(newsModelList,titleListDom,listClickHandler,highLight,selectedKey);
-    contentsView.renderContents(newsModelList,contentDom,selectedKey,removeNewsClickHandler);
+    menuView.render(newsModelList,headerDom,selectedKey,arrowClickHandler);
+    titleListView.render(newsModelList,titleListDom,listClickHandler,highLight,selectedKey);
+    contentsView.render(newsModelList,contentDom,selectedKey,removeNewsClickHandler);
 }
+
+
+
+
+
+
 
 //글자색 강조 함수
 function highLight(key) {
@@ -78,5 +96,5 @@ function highLight(key) {
     dom.forEach(function (value) {
         value.style.color = "black";
     });
-    utility.$selector("nav>ul>#\\3"+key).style.color = "blue";
+    utility.$selector("nav>ul>.\\3"+key).style.color = "blue";
 }
--- view/TitleListView.js
+++ view/TitleListView.js
@@ -2,26 +2,40 @@
  * Created by donghyunkim on 2017. 3. 16..
  */
 
+
+
+
+
+//모델리스트, 랜더링할 돔, 영향을 줄 dom, view, 핸들러
 var TitleListViewProtoType = {
 
     //titleView를 렌더링하는 메소드
-    renderTitleList : function(newsModelList,titleListDom,eventHandler,highlight,key){
-
-
+    render : function(renderingDom,modelList){
         let newsTitleListTemplate = utility.$selector('#newsTitleListTemplate').innerText;
-        let titleListData = this.getTitleList(newsModelList);
+        let titleListData = this.getTitleList(modelList);
 
         newsTitleListTemplate = newsTitleListTemplate.replace("{{newsList}}", titleListData.map(function (val) {
-            return "<li id="+ val.key +">" + val.title + "</li>"
+            return "<li class="+ val.key +">" + val.title + "</li>"
         }).join(""));
 
-        titleListDom.innerHTML = newsTitleListTemplate;
-        let listUl = utility.$selector("#titleUl");
-        listUl.addEventListener("click",eventHandler.bind(this,newsModelList))
+        let argArr = Array.prototype.slice.call(arguments);
+        renderingDom.innerHTML = newsTitleListTemplate;
 
-        if(highlight !== undefined && key !== undefined){
+        let highlight;
+        let key;
+
+        if(argArr[argArr.length-2] !== undefined && argArr[argArr.length-1] !== undefined){
+            highlight = argArr[argArr.length-2];
+            key = argArr[argArr.length-1];
             highlight(key);
         }
+
+        argArr.slice(1);
+        let listUl = utility.$selector(".titleUl");
+        //바인드를 apply사용하는 법
+        listUl.addEventListener("click",this.listClickHandler.bind.apply(this.listClickHandler,[this].concat(argArr.slice(1))));
+
+
     },
     //title 리스트를 가져오는 메소드
     getTitleList : function(newsModelList){
@@ -32,8 +46,23 @@ var TitleListViewProtoType = {
     },
     setTitleListViewKey : function(titleListViewKey){
         this.titleListViewKey = titleListViewKey;
+    },
+    listClickHandler : function (modelList) {
+     let key =  Number(event.target.className);
+     let argArr = Array.prototype.slice.call(arguments);
+        for(let i = 1; i < argArr.length-1; i++){
+            if(Object.prototype.toString.call(argArr[i]) !== "[object Object]"){
+                break;
+            }
+            let viewObj =  argArr[i].viewObj;
+            let domObj = argArr[i].domObj;
+            let handlerFn = argArr[i].handlerFn;
+            viewObj.render(modelList,domObj,key,handlerFn);
+            }
+        if(argArr[argArr.length-2] !== event) {
+            highLight(key);
+        }
     }
-
 };
 
 
